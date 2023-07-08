bf8e80c852226f282a7b981dd7bc26e6bc44ac53
kadunapikin <ibakoshi27@gmail.com>
2023-07-08 21:32:24 +0100

    create Card.js component and return Header.js and Body.js as childreen components

---
+++ my-webforum/src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Body from "./Body";
+import Header from "./Header";
+
+const Card = () => {
+    return (
+        <>
+        <Header />
+        <Body />
+        </>
+    )
+}
+export default Card;
